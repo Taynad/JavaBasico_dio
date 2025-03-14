c3df4149ded166e173030e4727bdd59fcd358592
Taynad <166086044+Taynad@users.noreply.github.com>
2025-03-13 21:42:29 -0300

    tratamento de exceções

--- estrutura-condionais/controle-fluxo/src/AboutMe.java
+++ estrutura-condionais/controle-fluxo/src/AboutMe.java
@@ -1,8 +1,11 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
+        try{
+            
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome: ");
@@ -21,6 +24,10 @@ public class AboutMe {
         System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome);
         System.out.println("Tenho " + idade + "anos");
         System.out.println("Minha altura é " + altura+ "cm ");
+        }catch(InputMismatchException e){
+            System.out.println("Os campos idade e altura precisam ser numéricos");
+        }
+        
 
     }
 }
