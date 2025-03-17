aad5c093c8edeeab843ec8db1694b12600dd7b79
Taynad <166086044+Taynad@users.noreply.github.com>
2025-03-17 16:16:22 -0300

    Update App.java

--- estrutura-condionais/controle-fluxo/src/App.java
+++ estrutura-condionais/controle-fluxo/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner scanner = new Scanner(System.in);
+        double[] transacoes = new double[3];
+
+        for(int i = 0; i < 3; i++){
+            transacoes[i] = scanner.nextDouble();
+        }
+
+        double valorMinimo = scanner.nextDouble();
+
+        int contador = 0;
+        for(int i =0; i < 3; i++){
+            if(transacoes[i] > valorMinimo){
+                contador++;
+            }
+        }
+
+        System.out.println(contador);
+        scanner.close();
+
+    }     
 }
