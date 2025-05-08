3d12a4fa27aa1242786d4dd44207915ea0a9bae8
Taynad <166086044+Taynad@users.noreply.github.com>
2025-05-08 18:16:43 -0300

    Update App.java

--- POO/exemplos-pacote/src/App.java
+++ POO/exemplos-pacote/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner scanner = new Scanner(System.in);
+       int valorA = scanner.nextInt();
+       int valorB = scanner.nextInt();
+       int valorC = scanner.nextInt();
+
+       if( valorA > valorB && valorA > valorC ){
+            System.out.println(valorA + " eh o maior");
+       }else if (valorB > valorA && valorB > valorC){
+            System.out.println(valorB + " eh o maior");
+       }else{
+            System.out.println(valorC + " eh o maior");
+       }
+
+       scanner.close();
+
+
+       
     }
 }
