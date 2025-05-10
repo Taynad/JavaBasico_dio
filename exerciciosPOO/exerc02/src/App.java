16003cefff1457ea53f0cdaf83a35db0f5ece668
Taynad <166086044+Taynad@users.noreply.github.com>
2025-05-10 17:05:40 -0300

    exer02 - carro finalizado

--- exerciciosPOO/exerc02/src/App.java
+++ exerciciosPOO/exerc02/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Carro carro = new Carro(0, 0, true);
+        carro.exibirMenu();
     }
 }
--- exerciciosPOO/exerc02/src/Carro.java
+++ exerciciosPOO/exerc02/src/Carro.java
@@ -31,7 +31,6 @@ public class Carro {
     }
 
     public void iniciarCarro(){
-        ligarCarro();
 
         if(desligado == true){
             System.out.println("Carro desligado, não é possível inciar");
@@ -42,19 +41,20 @@ public class Carro {
         }
     }
 
-    public void acelerar(){
-        iniciarCarro();
 
-        if(desligado == false && velocidade <= 120){
+    public void acelerar(){
+
+        if(desligado == false && velocidade <= 120 && marcha >= 1){
             velocidade += 1;
             System.out.println("Sua velocidade está em: " + velocidade + "Km");
-        }else{
-            System.out.println("Algo deu errado, tente novamente");
+        }else if(velocidade > 120){
+            System.out.println("Velocidade ultrapassou 120Km");
+        }else if(marcha <=0){
+            System.out.println("O carro está no ponto morto");
         }
     }
 
     public void diminuirVelocidade(){
-        iniciarCarro();
 
         if(desligado == false && velocidade >= 0){
             velocidade -= 1;
@@ -66,31 +66,95 @@ public class Carro {
     }
 
     public void virarEsquerdaDireita(){
-        iniciarCarro();
 
-        if(desligado == false){
+        if(desligado == false && velocidade >=1 && velocidade <= 40){
             System.out.println("Qual lado quer virar o carro?(direita ou esquerda)");
             String direcao = scanner.nextLine().toLowerCase().trim();
             if(direcao.equals("direita")){
                 System.out.println("Virando o carro para direita");
             }else if (direcao.equals("esquerda")){
                 System.out.println("Virando o carro pra esquerda");
-            }else{
-                System.out.println("Opção inválida");
+            }else if (velocidade > 40){
+                System.out.println("Velocidade acima do permitido para fazer a direção");
             }
         }else{
             System.out.println("Algo deu errado, tente novamente");
         }
     }
 
+    public void verificarVelocidade(){
+        System.out.println("Sua velocidade está em: " + velocidade +"Km");
+    }
+
 
     public void trocarMarcha(){
-        iniciarCarro();
-
         if (desligado == false) {
-            
+            if(marcha == 0){
+                marcha ++;
+                velocidade = 20;
+                System.out.println("Trocando de marcha...");
+                System.out.println("Você está na primeira marcha");
+            }else if(marcha == 1){
+                marcha ++;
+                velocidade = 40;
+                System.out.println("Trocando de marcha...");
+                System.out.println("Você está na segunda marcha");
+            }else if(marcha == 2){
+                marcha ++;
+                velocidade = 60;
+                System.out.println("Trocando de marcha...");
+                System.out.println("Você está na terceira marcha");
+            }else if(marcha == 3){
+                marcha ++;
+                velocidade = 80;
+                System.out.println("Trocando de marcha...");
+                System.out.println("Você está na quarta marcha");
+            }else if(marcha == 4){
+                marcha ++;
+                velocidade = 100;
+                System.out.println("Trocando de marcha...");
+                System.out.println("Você está na quinta marcha");
+            }else if(marcha == 5){
+                marcha ++;
+                velocidade = 120;
+                System.out.println("Trocando de marcha...");
+                System.out.println("Você está na sexta marcha");
+            }
+        }else{
+            System.out.println("Algo deu errado, tente novamente");
         }
         
+    }
+
+    public void exibirMenu(){
+        int opcao;
+        do{
+            System.out.println("\nMenu");
+            System.out.println("1. Ligar carro");
+            System.out.println("2. Iniciar carro");
+            System.out.println("3. Acelerar carro");
+            System.out.println("4. Diminuir velocidade do carro");
+            System.out.println("5. Direção do carro");
+            System.out.println("6. Verificar velocidade");
+            System.out.println("7. Trocar marcha");
+            System.out.println("8. Sair");
+            System.out.println("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1 -> ligarCarro();
+                case 2 -> iniciarCarro();
+                case 3 -> acelerar();
+                case 4 -> diminuirVelocidade();
+                case 5 -> virarEsquerdaDireita();
+                case 6 -> verificarVelocidade();
+                case 7 -> trocarMarcha();
+                case 8 -> System.out.println("Saindo....");
+                default -> System.out.println("Opção inválida.");
+                    
+            }
+        }while (opcao != 8);
     }
 
 
