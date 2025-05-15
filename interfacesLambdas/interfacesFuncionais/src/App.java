47ae7b8faed2b50fe9952a9490959825f3f0694b
Taynad <166086044+Taynad@users.noreply.github.com>
2025-05-15 14:50:26 -0300

    interface funcional

---
+++ interfacesLambdas/interfacesFuncionais/src/App.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        List<Users> users = List.of(new Users("Maria", 21),
+        new Users("Cecília", 40), new Users("Tayna", 65));
+    }
+}
---
+++ interfacesLambdas/interfacesFuncionais/src/Users.java
@@ -0,0 +1,3 @@
+public record Users(String name, int age) {
+
+}
