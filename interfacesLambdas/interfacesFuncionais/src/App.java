640eec588b6ae9bab8b3465b691e56e7d1ff25bf
Taynad <166086044+Taynad@users.noreply.github.com>
2025-05-15 16:35:00 -0300

    Update App.java

--- interfacesLambdas/interfacesFuncionais/src/App.java
+++ interfacesLambdas/interfacesFuncionais/src/App.java
@@ -2,7 +2,12 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //lista imutável
         List<Users> users = List.of(new Users("Maria", 21),
         new Users("Cecília", 40), new Users("Tayna", 65));
+        //expressão lambda
+        users.forEach((Users user) -> {
+            System.out.println(user);
+        });
     }
 }
