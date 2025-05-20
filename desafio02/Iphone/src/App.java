560446ed544574a910cd22fe5b09a15aaea7ddc2
Taynad <166086044+Taynad@users.noreply.github.com>
2025-05-20 16:09:26 -0300

    desafio02 - iphone

---
+++ desafio02/Iphone/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Iphone iphone = new Iphone();
+        iphone.adicionarNovaAba();
+        iphone.atender();
+        iphone.ligar("12123344");
+    }
+}
---
+++ desafio02/Iphone/src/NavegadorInternet.java
@@ -0,0 +1,6 @@
+public interface NavegadorInternet {
+    public abstract void exibirPagina(String url);
+    public abstract void adicionarNovaAba();
+    public abstract void atualizarPagina();
+
+}
