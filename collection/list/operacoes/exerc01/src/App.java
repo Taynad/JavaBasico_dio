87de1e5ab24d74878d9e188c5ef77b9b2e8f5442
Taynad <166086044+Taynad@users.noreply.github.com>
2025-06-23 17:52:58 -0300

    exercicio 1 - list

---
+++ collection/list/operacoes/exerc01/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+        
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+    }
+}
---
+++ collection/list/operacoes/exerc01/src/Tarefa.java
@@ -0,0 +1,24 @@
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    //construtor
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    //getter
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+
+    
+
+    
+
+}
