be0e63682d3333ecba69c4a8d232a660b00ee755
Taynad <166086044+Taynad@users.noreply.github.com>
2025-06-23 18:42:15 -0300

    exercicio 03 - list

---
+++ collection/list/pesquisa/exerc01/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        // Teste 1: Adicionar livros
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020, 2022));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+}
