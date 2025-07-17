53a694cb9012b650556e169624d4ac1b956058f5
Taynad <166086044+Taynad@users.noreply.github.com>
2025-07-17 20:37:17 -0300

    Update App.java

--- streamAPI/exercicios/exerciciosStream/src/App.java
+++ streamAPI/exercicios/exerciciosStream/src/App.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -63,7 +64,68 @@ public class App {
         /*
          * Encontrar o segundo número maior da lista
          */
-        
+        System.out.println("----EXERC 7----");
+        Optional<Integer> segundoMaior = numeros.stream()
+                .distinct() // remove duplicados
+                .sorted((a, b) -> b.compareTo(a)) // ordena decrescente
+                .skip(1) // pula o maior
+                .findFirst(); // pega o próximo (segundo maior)
+
+        if (segundoMaior.isPresent()) {
+            System.out.println("O segundo maior número é: " + segundoMaior.get());
+        } else {
+            System.out.println("Não existe segundo maior número na lista.");
+        }
+
+        /*
+         * Somar os dígitos de todos os números da lista
+         */
+        System.out.println("----EXERC 8----");
+        int somaLista = numeros.stream()
+            .reduce(0, Integer::sum);
+        System.out.println(somaLista);
+
+        /*
+         * Verificar se todos os números da lista são distintos (não se repetem)
+         */
+        System.out.println("----EXERC 9----");
+        boolean numerosRepetem = numeros.stream()
+            .allMatch(n -> n == n);
+        System.out.println(numerosRepetem);
+
+        /*
+         * Agrupe os valores ímpares múltiplos de 3 ou de 5
+         */
+        System.out.println("----EXERC 10----");
+        List<Integer> multiplos = numeros.stream()
+            .filter(n -> n % 3 == 0 || n % 5 == 0)
+            .collect(Collectors.toList());
+        System.out.println(multiplos);
+
+        /*
+         * Encontre a soma dos quadrados de todos os números da lista
+         */
+        System.out.println("----EXERC 11----");
+        List<Integer> somaQuadrados = numeros.stream()
+            .map(n -> n * n)
+            .collect(Collectors.toList());
+        System.out.println(somaQuadrados);
+
+        /*
+         * Encontre o produto de todos os números da lista
+         */
+        System.out.println("----EXERC 12----");
+        int produtoLista = numeros.stream()
+            .reduce(1, (a, b) -> a * b);
+        System.out.println(produtoLista);
+        /*
+         * Filtrar os números que estão dentro de um intervalo
+         */
+        System.out.println("----EXERC 13----");
+        List<Integer> intervaloLista = numeros.stream()
+            .filter(n -> n > 5 && n < 10)
+            .collect(Collectors.toList());
+        System.out.println(intervaloLista);
 
 
         
